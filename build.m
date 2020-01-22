dddd1fe0576fa17201e3f58cd55065746f462c08
Jed <jed@m6700.local>
2020-01-21 20:25:49 -0500

    .

--- build.m
+++ build.m
@@ -1,18 +1,21 @@
 %% Automatically Generated Run Script
 failed=0;
 try
-    fprintf('########## Started ##########\n');
+    profile('on'); % Start profiling.
     
+    % Log Messages
+    fprintf('########## Started ##########\n');
     fprintf('########## Jenkins Environment ##########\n');
+    % Print all Jenkins Variables.
     for jenkins_var = {'BUILD_NUMBER', 'BUILD_ID', 'BUILD_URL', 'NODE_NAME', 'JOB_NAME', 'BUILD_TAG', 'JENKINS_URL', 'EXECUTOR_NUMBER', 'JAVA_HOME', 'WORKSPACE', 'GIT_COMMIT', 'GIT_URL', 'GIT_BRANCH'}
         fprintf('%% %s: %s\n',jenkins_var{1}, getenv(jenkins_var{1}));
     end
-    
-    
+    % Start build process.
     fprintf('########## Build Process ##########\n');
     % restoredefaultpath;
-    disp('% TODO: Build Stuff')
+    buildArduinoTemplates;
 catch me
+    % Dump the full stack for debugging.
     fprintf('########## Failed ##########\n');
     fprintf('ERROR: %s (%s)\n\n',me.message, me.identifier)
     for i = numel(me.stack):-1:1
@@ -21,6 +24,8 @@ catch me
     failed=1;
 end
 fprintf('########## Finished ##########\n');
+% Save profiling results.
+profsave(profile('info'),'profile_results')
 if ~isempty(getenv('WORKSPACE'))
     % Only exit running under Jenkins.
     exit(failed);
---
+++ buildArduinoTemplates.m
@@ -0,0 +1,36 @@
+function buildArduinoTemplates(varargin)
+bdclose('all');
+load_system('simulink')
+mkdir('build')
+cd('build');
+
+%% Build Templates
+template_base = 'arduino_BasicModel.sltx';
+template_directory = fileparts(which(template_base));
+templates = dir(fullfile(template_directory, '*.sltx'));
+%% Build Examples
+% Model name to search fore.
+example_base = 'arduino_wifi';
+% Find the template directory.
+example_directory = fileparts(which(example_base));
+% Find all of the example models.
+examples = dir(fullfile(example_directory, '*.slx'));
+%%
+% for template in templates:
+for template = [templates;examples]'
+    [~, model] = fileparts(template.name);
+    hdl = Simulink.createFromTemplate(model);
+    
+
+    cs = getActiveConfigSet(hdl);
+    cs.set_param('GenCodeOnly', 'on');   % Generate code only
+    % Save the system
+    save_system(hdl, sprintf('%s_%s', model, 'ci'));
+    % Build
+    slbuild(hdl);
+    % cs.buildRTW;
+    
+    % Save the system and close it.
+    save_system(hdl);
+    close_system(hdl);
+end
